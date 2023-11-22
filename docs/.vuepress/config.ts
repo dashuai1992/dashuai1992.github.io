6aec6277800a488aaa6b13b8570239dd254c7648
yuandashuai <yuandashuai@hualala.com>
2023-11-23 02:26:39 +0800

    comments

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -255,11 +255,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: '60b1962097f74eb9c5cb',
+          clientSecret: '6cc6403523b1336cb5a1c688102602cce0b3a42d',
+          repo: 'dashuai1992.github.io', // GitHub 仓库
+          owner: 'dashuai1992', // GitHub仓库所有者
+          admin: ['dashuai1992'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
